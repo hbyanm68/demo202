abd142771ddf6b0c31f14461f2afc01805feb121
hbyanmin <5469341+hbyanmin@user.noreply.gitee.com>
2022-07-22 15:48:01 +0800

    第一次代码

---
+++ src/main/java/kgc/cn/Demo.java
@@ -0,0 +1,13 @@
+package kgc.cn;
+
+/**
+ * @Author: yan.
+ * @Description:
+ * @Date:Created in 2022/7/22 15:47.
+ * @Modified By:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello 123");
+    }
+}
